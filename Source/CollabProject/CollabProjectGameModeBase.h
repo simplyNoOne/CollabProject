d183cadb9ad8d78da6c8d006fe7a90d1083030ae
Ania000 <annasidor00@gmail.com>
2022-04-23 18:14:11 +0200

    Initial Submit

---
+++ Source/CollabProject/CollabProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CollabProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLABPROJECT_API ACollabProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
